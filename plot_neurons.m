b5577e85495d37caa02b5da073ae80558f2e45e6
Anders Glent Buch <andersgb1@gmail.com>
2016-04-28 20:06:55 +0200

    Allow input strengths and use silent figures

--- plot_neurons.m
+++ plot_neurons.m
@@ -5,29 +5,41 @@ assert(round(rows)==rows, 'Non-integer number of rows!')
 p = inputParser;
 p.CaseSensitive = false;
 p.addParameter('Strongest', true, @islogical)
+p.addParameter('Strengths', [], @isfloat)
+p.addParameter('Name', '', @ischar)
 p.parse(varargin{:});
 strongest = p.Results.Strongest;
+strengths = p.Results.Strengths;
+name = p.Results.Name;
 
 [dim, N] = size(W);
 height = dim / width;
 assert(round(width) == width, 'Non-integer image width!');
 assert(round(height) == height, 'Non-integer image height!');
 num_plots = min(rows*rows, N);
-strengths = zeros(1,N);
-for i=1:N, strengths(i) = norm(W(:,i)); end
+if isempty(strengths)
+    strengths = zeros(1,N);
+    for i=1:N, strengths(i) = norm(W(:,i)); end
+else
+    assert(numel(strengths)==N);
+end
 if strongest
     [~, idx] = sort(strengths, 'descend');
-    figname = sprintf('%i strongest units', num_plots);
+    if isempty(name), name = sprintf('%i strongest units', num_plots); end
 else
     [~, idx] = sort(strengths, 'ascend');
-    figname = sprintf('%i weakest units', num_plots);
+    if isempty(name), name = sprintf('%i weakest units', num_plots); end
 end
 
+if isempty(name), name = sprintf('%i units', num_plots); end
+
 % Create fig
-h = findobj('type', 'figure', 'name', figname);
-if isempty(h), h = figure('Name', figname); end
-figure(h)
-% h = figure('Name', figname);
+h = findobj('type', 'figure', 'name', name);
+if isempty(h)
+    h = figure('Name', name);
+else
+    set(0, 'CurrentFigure', h);
+end
 
 % Plot
 for i = 1:num_plots
